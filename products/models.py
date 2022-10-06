f7cf56f3f47695b3ff5d1bcd9936016b61402614
Armando Beltran <armando_b@live.com>
2022-10-06 11:09:04 -0500

    Created Product Model Class with property fields + Changed settings file to convert strings to integer for decimals

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField()
+    inventory_quantity = models.IntegerField(max_digits=8, decimal_places=2)

0674b6e01cdb769a46448df5a7e8a65dd0343be3
Armando Beltran <armando_b@live.com>
2022-10-06 10:29:06 -0500

    Changed Database Settings + Created Products app

--- products_project/settings.py
+++ products_project/settings.py
@@ -68,10 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'products_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-
 
 
 # Password validation
@@ -114,3 +110,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from products_project.local_settings import *
+except ImportError:
+    pass

6273e6cb14fa6a7490c32912ec8de7be03360628
Armando Beltran <armando_b@live.com>
2022-10-07 13:40:48 -0500

    Created New View Function for Product Detail and by ID + Added New URLs path for product_detail view function

--- products/views.py
+++ products/views.py
@@ -1,8 +1,30 @@
+from http.client import CannotSendRequest
+from math import prod
+from django.urls import is_valid_path
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from.models import Product
+from products import serializers
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def products_list(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET'])
-def products_list(request):
-
-
-    return Response('ok')
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)

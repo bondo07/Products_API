83f1072b8a460427a70e5fab36fdf25bdd2ef3f6
Armando Beltran <armando_b@live.com>
2022-10-07 14:33:17 -0500

    Created Update/Put functionality for Products in product_app views file

--- products/views.py
+++ products/views.py
@@ -1,12 +1,10 @@
-from http.client import CannotSendRequest
-from math import prod
-from django.urls import is_valid_path
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductSerializer
 from.models import Product
-from products import serializers
+
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def products_list(request):
@@ -20,11 +18,14 @@ def products_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def product_detail(request, pk):
-    try:
-        product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    except Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
